dep updates + overflow fix

catprinter/img.py
@@ -4,10 +4,9 @@ import numpy as np
 
 from catprinter import logger
 
-
 def floyd_steinberg_dither(img):
-    '''Applies the Floyd-Steinberf dithering to img, in place.
-    img is expected to be a 8-bit grayscale image.
+    '''Applies the Floyd-Steinberg dithering to img, in place.
+    img is expected to be an 8-bit grayscale image.
 
     Algorithm borrowed from wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering.
     '''
@@ -16,22 +15,23 @@ def floyd_steinberg_dither(img):
     def adjust_pixel(y, x, delta):
         if y < 0 or y >= h or x < 0 or x >= w:
             return
-        img[y][x] = min(255, max(0, img[y][x] + delta))
+        img[y][x] = np.clip(int(img[y][x]) + delta, 0, 255)
 
     for y in range(h):
         for x in range(w):
-            new_val = 255 if img[y][x] > 127 else 0
-            err = img[y][x] - new_val
+            old_val = int(img[y][x])
+            new_val = 255 if old_val > 127 else 0
+            err = old_val - new_val
             img[y][x] = new_val
-            adjust_pixel(y, x + 1, err * 7/16)
-            adjust_pixel(y + 1, x - 1, err * 3/16)
-            adjust_pixel(y + 1, x, err * 5/16)
-            adjust_pixel(y + 1, x + 1, err * 1/16)
+            adjust_pixel(y, x + 1, err * 7 // 16)
+            adjust_pixel(y + 1, x - 1, err * 3 // 16)
+            adjust_pixel(y + 1, x, err * 5 // 16)
+            adjust_pixel(y + 1, x + 1, err * 1 // 16)
     return img
 
 
 def halftone_dither(img):
-    '''Applies Haltone dithering using different sized circles
+    '''Applies Halftone dithering using different sized circles
 
     Algorithm is borrowed from https://github.com/GravO8/halftone
     '''